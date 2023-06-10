62c30ad4d910e9b299a9bdf5a59da2622df47b6e
Barry Deng <dengli.bg@gmail.com>
2023-06-10 23:09:57 +0800

    update some compile flags and architectures to add support for Xcode 15 beta and iOS simulator.

--- ios/prepare_libs.sh
+++ ios/prepare_libs.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -euxo pipefail
 
+export MODEL=dolly-v2-3b
+export QUANTIZATION=q3f16_0
+
 rustup target add aarch64-apple-ios
+rustup target add x86_64-apple-ios
 
 mkdir -p build/ && cd build/
 
 cmake ../..\
   -DCMAKE_BUILD_TYPE=Release\
   -DCMAKE_SYSTEM_NAME=iOS\
-  -DCMAKE_SYSTEM_VERSION=14.0\
+  -DCMAKE_SYSTEM_VERSION=16.0\
   -DCMAKE_OSX_SYSROOT=iphoneos\
-  -DCMAKE_OSX_ARCHITECTURES=arm64\
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=14.0\
+  -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"\
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=16.0\
   -DCMAKE_BUILD_WITH_INSTALL_NAME_DIR=ON\
   -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=ON\
   -DCMAKE_INSTALL_PREFIX=.\
--- ios/prepare_params.sh
+++ ios/prepare_params.sh
@@ -6,7 +6,8 @@ rm -r dist
 mkdir -p dist
 
 declare -a builtin_list=(
-    "RedPajama-INCITE-Chat-3B-v1-q4f16_0"
+    "dolly-v2-3b-q3f16_0"
+    # "RedPajama-INCITE-Chat-3B-v1-q4f16_0"
     # "vicuna-v1-7b-q3f16_0"
     # "rwkv-raven-1b5-q8f16_0"
     # "rwkv-raven-3b-q8f16_0"
